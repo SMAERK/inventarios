fec79bd4225863aa615c0a9c3ae82eb598dd3187
SMAERK <juliethcontreras28@hotmail.colm>
2022-01-03 22:37:29 -0500

    gestion de inventarios

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?php
+// PRODUCCION
+// $cfg->servidor = "exisimplexdb.cji1xbl0xigt.us-east-1.rds.amazonaws.com";
+// $cfg->puerto = 5432;
+// $cfg->nombreDeUsuario = "postgres";
+// $cfg->contrasena = "ZxQaz2020**";
+// $cfg->nombreBD = "simplexweb_ss_continautos";
+?>
